35756c567f17428a3512c6ecd3d73e990318b39a
Gabriel Oliveira <goliveira182826@gmail.com>
2024-02-24 00:26:10 -0300

    deved i

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 const botoes = $(".botao");
 
 for (let i = 0; i < botoes.length; i++) {
-    botoes[i].onclick = function() {
+    $(botoes[i]).click(function() {
         for (let j = 0; j < botoes.length; j++) {
             $(botoes[j]).removeClass('active');
         }
-        $(botoes[i]).addClass('active');
-    };
+        $(this).addClass('active');
+    });
 }
