6550ce98fcc8f19c7f1fd56cebf850b90470f4ae
Gabriel Oliveira <goliveira182826@gmail.com>
2024-02-24 00:35:08 -0300

    agr vai

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-console.log(funcionou);
+console.log("funcionou");
 
-const botoes = document.querySelectorAll(".botao");
+const botoes = document.querySelectorAll(".btn");
 
-for(let i=0;i <botoes.length;i++){
-    botoes[i].onclick = function(){
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
 
-        for(let j=0;j<botoes.length;j++){
+        for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("active");
         }
 
